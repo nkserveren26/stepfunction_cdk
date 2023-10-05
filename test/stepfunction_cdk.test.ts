88a616361387c84bc6890000f705ff8765ece4cf
nkserveren26 <norinoritoshi@outlook.jp>
2023-10-05 09:17:37 +0900

    add cdk resources

---
+++ test/stepfunction_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StepfunctionCdk from '../lib/stepfunction_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/stepfunction_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StepfunctionCdk.StepfunctionCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
